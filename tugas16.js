346e33480636279d60d0adc2e1812295546b1154
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:23:46 +0700

    tugas niomic

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+function tugas16(){
+    let min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    let max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    console.log("Nilai Minimal : " + min);
+    console.log("Nilai Maksimal : " + max);
+};
+
+tugas16();
